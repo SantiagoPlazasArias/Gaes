bc34ea1705ccc9a6f01ab6d9beea6f55d2785924
mauro-skibdi <mmauroalexandermartinez49@gmail.com>
2025-05-22 07:59:18 -0500

    a

--- LotusDream/js/script.js
+++ LotusDream/js/script.js
@@ -20,22 +20,41 @@ function render(){
         document.getElementById('contentDisplay').innerHTML = '<p>No hay servicios disponibles en esta categoría.</p>';
         return;
     
-    }
+    }/*
+        html = ''
+    servicios.forEach(servicio => {
+    html += `
+  <div class="card mb-4 rounded-3 shadow-sm">
+    <div class="card-header py-3">
+      <h4>${servicio.nombre}</h4>
+    </div>
+    <div class="card-body">
+      <img class="bd-placeholder-img card-img-top" src="${servicio.imagen}" alt="${servicio.nombre}" width="100%">
+      ${servicio.presentacion ? `<p>${servicio.presentacion}</p>` : ''}
+      <p>$${servicio.precio.toLocaleString('es-CO')}</p>
+      <p>${servicio.descripcion}</p>
+      <button class="btn-mas-info">
+        <a href="Descripciones/manicuratradicional.html"> Más Información </a>
+      </button>
+    </div>
+  </div>
+`;
+    });*/
     html = ''
     servicios.forEach(servicio => {
             html += `
-        <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3">
-        <h4>${servicio.nombre}</h4> </div>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"">
+        <div class="card shadow-sm">
+        <h4>${servicio.nombre}</h4> 
         <div class="card-body">
-            <img src="${servicio.imagen}" alt="${servicio.nombre}" width='10%'>
-        </div>
+          <img class="bd-placeholder-img card-img-top" src="${servicio.imagen}" alt="${servicio.nombre}" width='10em' height='10em'>        </div>
         ${servicio.presentacion ? `<p>${servicio.presentacion}</p>` : ''}
         <p>$${servicio.precio.toLocaleString('es-CO')}</p>
         <p>${servicio.descripcion}</p>
         <button class="btn-mas-info">
           <a href="Descripciones/manicuratradicional.html"> Más Información </a>
         </button>
+      </div>
       </div>
     `;
     });
